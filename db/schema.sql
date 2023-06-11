05ef2a4597c1e1de1ea7b17e98aab6fa04c18158
lexslie <lesliiee727@gmail.com>
2023-06-10 20:20:27 -0700

    update schema.sql
    minor numerical adjustment

--- db/schema.sql
+++ db/schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10,2) NOT NULL, 
+    salary DECIMAL(10.3) NOT NULL, 
     department_id INT NOT NULL,
     PRIMARY KEY (id)
 );
